4f675d8e771710d47fbcda6502478c0beba507dc
Aaron Boatwright <boatwrightaaron@gmail.com>
2017-04-19 13:59:38 -0700

    adds completed exercises: 1, 2, 3 , thanks to Jacky

--- mag-part-1/compose/compose_exercises.js
+++ mag-part-1/compose/compose_exercises.js
@@ -1,7 +1,7 @@
 require('../support');
 var _ = require('ramda');
 var accounting = require('accounting');
-  
+
 // Example Data
 var CARS = [
     {name: "Ferrari FF", horsepower: 660, dollar_value: 700000, in_stock: true},
@@ -11,30 +11,35 @@ var CARS = [
     {name: "Aston Martin One-77", horsepower: 750, dollar_value: 1850000, in_stock: true},
     {name: "Pagani Huayra", horsepower: 700, dollar_value: 1300000, in_stock: false}
   ];
+  // Exercise 1:
+  // ============
+  // use _.compose() to rewrite the function below. Hint: _.prop() is curried.
+  // var isLastInStock = function(cars) {
+  //   var reversed_cars = _.last(cars);
+  //   return _.prop('in_stock', reversed_cars)
+  // };
 
-// Exercise 1:
-// ============
-// use _.compose() to rewrite the function below. Hint: _.prop() is curried.
-var isLastInStock = function(cars) {
-  var reversed_cars = _.last(cars);
-  return _.prop('in_stock', reversed_cars)
-};
-
-// Exercise 2:
-// ============
-// use _.compose(), _.prop() and _.head() to retrieve the name of the first car
-var nameOfFirstCar = undefined;
+var isLastInStock = _.compose( _.prop('in_stock'), _.last)
 
 
-// Exercise 3:
-// ============
-// Use the helper function _average to refactor averageDollarValue as a composition
+  // Exercise 2:
+  // ============
+  // use _.compose(), _.prop() and _.head() to retrieve the name of the first car
+  // var nameOfFirstCar = undefined;
+
+var nameOfFirstCar = _.compose(_.prop('name'), _.head)
+
+  // Exercise 3:
+  // ============
+  // Use the helper function _average to refactor averageDollarValue as a composition
 var _average = function(xs) { return reduce(add, 0, xs) / xs.length; }; // <- leave be
 
-var averageDollarValue = function(cars) {
-  var dollar_values = map(function(c) { return c.dollar_value; }, cars);
-  return _average(dollar_values);
-};
+  // var averageDollarValue = function(cars) {
+  //   var dollar_values = map(function(c) { return c.dollar_value; }, cars);
+  //   return _average(dollar_values);
+  // };
+
+var averageDollarValue = _.compose( _average, _.map(_.prop('dollar_value')))
 
 
 // Exercise 4:
